fba793c9b998e19a0c32173aa940d6c07d344bc1
Jinsu Ha <jinsu.jh.ha@gmail.com>
2021-06-22 13:23:11 -0400

    update oauth2

--- common.py
+++ common.py
@@ -1,15 +1,15 @@
-from googleapiclient import discovery
-from httplib2 import Http
-from oauth2client import file, client, tools
-
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 # Headers in Athlete data
 STR_NAME = "Name"
 STR_DATE = "Date"
 
 # Authorization constants
-CLIENT_ID_FILE = "google-credentials.json"
-TOKEN_STORE_FILE = "token.json"
+CLIENT_SECRETS_FILE = "google-credentials.json"
 SCOPES = (  # iterable or space-delimited string
     "https://www.googleapis.com/auth/drive",
     "https://www.googleapis.com/auth/documents",
@@ -17,21 +17,34 @@ SCOPES = (  # iterable or space-delimited string
 )
 
 
-def get_http_client():
+def get_credentials():
     """
-    Uses project credentials in CLIENT_ID_FILE along with requested OAuth2
+    Uses project credentials in CLIENT_SECRETS_FILE along with requested OAuth2
     scopes for authorization, and caches API tokens in TOKEN_STORE_FILE.
     """
-    store = file.Storage(TOKEN_STORE_FILE)
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_ID_FILE, SCOPES)
-        creds = tools.run_flow(flow, store)
-    return creds.authorize(Http()), creds
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
 
 
 # Service endpoints to Google APIs
-HTTP, CREDS = get_http_client()
-DRIVE = discovery.build("drive", "v3", http=HTTP)
-DOCS = discovery.build("docs", "v1", http=HTTP)
-SHEETS = discovery.build("sheets", "v4", http=HTTP)
+CREDS = get_credentials()
+DRIVE = build("drive", "v3", credentials=CREDS)
+DOCS = build("docs", "v1", credentials=CREDS)
+SHEETS = build("sheets", "v4", credentials=CREDS)
